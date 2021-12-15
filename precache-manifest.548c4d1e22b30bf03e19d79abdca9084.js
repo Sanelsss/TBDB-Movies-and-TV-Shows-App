5ba4338bcfadf079d9bfcbb3bbe1df35af0e7b8d
Sanelsss <sanel_omanovic@hotmail.com>
2021-12-15 03:08:27 +0100

    Updates

---
+++ precache-manifest.548c4d1e22b30bf03e19d79abdca9084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abff6608c6c172ccfa758ca2b17fbd8d",
+    "url": "/TBDB-Movies-and-TV-Shows-App/index.html"
+  },
+  {
+    "revision": "48546b0acf47e814a159",
+    "url": "/TBDB-Movies-and-TV-Shows-App/static/css/main.7b3c70f0.chunk.css"
+  },
+  {
+    "revision": "d32475b03f10990432f7",
+    "url": "/TBDB-Movies-and-TV-Shows-App/static/js/2.b612a5f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TBDB-Movies-and-TV-Shows-App/static/js/2.b612a5f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48546b0acf47e814a159",
+    "url": "/TBDB-Movies-and-TV-Shows-App/static/js/main.50f10310.chunk.js"
+  },
+  {
+    "revision": "5643ef7835b7af3bcfbf",
+    "url": "/TBDB-Movies-and-TV-Shows-App/static/js/runtime-main.d2760cfe.js"
+  }
+]);
